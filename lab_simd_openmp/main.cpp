9293908f1b87a8159c41059e4bd14a12e56afca0
nikitaurasov <n.yurasov@yahoo.com>
2021-10-09 12:18:53 +0300

    added avx2 vectorization

--- lab_simd_openmp/main.cpp
+++ lab_simd_openmp/main.cpp
@@ -4,7 +4,8 @@
 #include <cmath>
 #include <immintrin.h>
 #include <x86intrin.h>
-
+#include <xmmintrin.h>
+#include <avx2intrin.h>
 #include "timing.hpp"
 
 
@@ -12,12 +13,21 @@ std::vector<double> a(MAX_SIZE);
 std::vector<double> b(MAX_SIZE);
 
 
-void vectorized_sum(size_t n) {
+void vectorized_sum_sse(size_t n) {
     for (size_t i = 0; i < n; i += 2) {
         __m128d x = _mm_loadu_pd(&a[i]);
         __m128d y = _mm_loadu_pd(&b[i]);
         x = _mm_add_pd(x, y);
         _mm_store_si128((__m128i *) &a[i], x);
+    }
+}
+
+void vectorized_sum_avx2(size_t n) {
+    for (size_t i = 0; i < n; i += 4) {
+        __m256d x = _mm256_loadu_pd(&a[i]);
+        __m256d y = _mm256_loadu_pd(&b[i]);
+        x = _mm256_add_pd(x, y);
+        _mm256_store_pd(&a[i], x);
     }
 }
 
@@ -51,7 +61,7 @@ double numeric_integration_omp(int n, double a_bound, double b_bound, arithmetic
 double numeric_integration_simd(int n, double a_bound, double b_bound, arithmetic_function_type f) {
     double _sum = 0.0;
     double dx = (b_bound - a_bound) / n;
-    #pragma omp simd reduction(+:_sum)
+    #pragma omp simd share(_sum) reduction(+:_sum)
     for (int i = 0; i < n; ++i) {
         _sum += dx * f(dx * i);
     }
@@ -66,6 +76,13 @@ double exp_x2(double x) {
 
 
 int main() {
+    std::cout << "sse: " << __builtin_cpu_supports("sse") << "\n";
+    std::cout << "sse2: " << __builtin_cpu_supports("sse2") << "\n";
+    std::cout << "avx: " << __builtin_cpu_supports("avx") << "\n";
+    std::cout << "avx2: " << __builtin_cpu_supports("avx2") << "\n";
+    std::cout << "avx512f: " << __builtin_cpu_supports("avx512f") << "\n";
+
+
     std::fill(std::begin(a), std::begin(a) + MAX_SIZE, (double) 3e3);
     std::fill(std::begin(b), std::begin(b) + MAX_SIZE, (double) -1.123e3);
 
@@ -76,9 +93,16 @@ int main() {
         printf("%-10d %7.2f\n", i, (double) t / (double) i);
     }
     std::cout << "\n++++++++++++++++++++++++++++++++++++++++\n";
-    printf("%-10s Cycles/Element for %s\n", "Size", "Vec Sum");
+    printf("%-10s Cycles/Element for %s\n", "Size", "Vec Sum SSE2");
     for (size_t i = 16; i <= MAX_SIZE; i *= 2) {
-        uint64_t t = measure_function_intrinsic(vectorized_sum, i);
+        uint64_t t = measure_function_intrinsic(vectorized_sum_sse, i);
+        printf("%-10d %7.2f\n", i, (double) t / (double) i);
+    }
+
+    std::cout << "\n++++++++++++++++++++++++++++++++++++++++\n";
+    printf("%-10s Cycles/Element for %s\n", "Size", "Vec Sum AVX2");
+    for (size_t i = 16; i <= MAX_SIZE; i *= 2) {
+        uint64_t t = measure_function_intrinsic(vectorized_sum_avx2, i);
         printf("%-10d %7.2f\n", i, (double) t / (double) i);
     }
 
